dfca1f23b06bfb1b1e4e5060f6022ff94cbc83ca
Feruz <70520745+Feruz-Islomov@users.noreply.github.com>
2023-07-04 21:37:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(
-  cors("https://itchy-pants-bee.cyclic.app/api/postproduct")
+  cors("*")
 );
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
